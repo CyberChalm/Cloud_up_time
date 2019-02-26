a82d4668be50f3fc82bbfcdc463c591a7a37c1fd
CyberChalm <45452716+CyberChalm@users.noreply.github.com>
2019-02-26 22:54:39 +0000

    Add wall command and readme (#2)

--- cloud_up_time.sh
+++ cloud_up_time.sh
@@ -4,12 +4,9 @@ upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
 
 if [ "${upSeconds}" -gt "14400" ]
 then
-	printf 'This system has been up for: %dh:%dm:%ds\n' $(($upSeconds/3600)) $(($upSeconds%3600/60)) $(($upSeconds%60))
-	printf 'To prevent unnecessary costs this system will shutdown in 60 seconds. To cancel this action please type "shutdown -c"'
-	sleep 60
+	printf 'This system has been up for: %dh:%dm:%ds. To prevent unnecessary costs this system will shutdown in 5 minutes. To cancel this action please type "shutdown -c"\n' $(($upSeconds/3600)) $(($upSeconds%3600/60)) $(($upSeconds%60))| wall
+	sleep 300
 	/sbin/shutdown -h
-
 else
     printf 'This system has been up for: %dh:%dm:%ds\n' $(($upSeconds/3600)) $(($upSeconds%3600/60)) $(($upSeconds%60))
-
 fi
